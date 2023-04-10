e0aeb634ddae10d55f74a4d9cc87281166f88db5
Vince <vmorrill@appliedsystems.com>
2023-04-10 17:26:42 -0500

    Added Cost method

--- OperaHouse-Assignment3/ConcessionSale.cs
+++ OperaHouse-Assignment3/ConcessionSale.cs
@@ -4,16 +4,18 @@ namespace OperaHouse_Assignment3
 {
     public class ConcessionSale
     {
-        public double Price { get; set; }
+        public double UnitPrice { get; set; }
         public int Quantity { get; private set; }
 
         public string Description { get; private set; }
 
         public ConcessionSale(double price, int quantity, string description)
         {
-            Price = price;
+            UnitPrice = price;
             Quantity = quantity;
             Description = description;
         }
+
+        public double Cost() { return Quantity * UnitPrice; }
     }
 }
