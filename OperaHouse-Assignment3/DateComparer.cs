5b2eedb7eca739541476bdb4d3812190cc47c453
Nathaniel Kuehn <kuehnn91@uwosh.edu>
2023-04-18 10:57:35 -0500

    fixed curly brace

--- OperaHouse-Assignment3/DateComparer.cs
+++ OperaHouse-Assignment3/DateComparer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OperaHouse_Assignment3;
 
 namespace OperaHouse_Assignment3
 {
@@ -13,4 +14,48 @@ namespace OperaHouse_Assignment3
             return x.EventTime.CompareTo(y.EventTime);
         }
     }
-}
+    
+    
+    
+
+
+class EventComparer : IComparer<Event>
+{
+    public enum SortBy { EventTime, NumAvaliableTickets, Title, Performer };
+
+    public SortBy SortOrder { get; set; } = SortBy.EventTime;
+    public bool Descending { get; set; } = false;
+    internal int CompareTo(Performer performer)
+    {
+       
+    }
+
+    public int Compare(Event x, Event y)
+    {
+        int result = 0;
+        switch (SortOrder)
+        {
+            case SortBy.EventTime:
+                result = x.EventTime.CompareTo(y.EventTime);
+                break;
+            case SortBy.NumAvaliableTickets:
+                result = x.NumAvailableTickets.CompareTo(y.NumAvailableTickets);
+                break;
+            case SortBy.Title:
+                result = x.Title.CompareTo(y.Title);
+                break;
+            case SortBy.Performer:
+                result = x.Performer.CompareTo(y.Performer);
+                break;
+            default:
+                throw new ArgumentException("Invalid sort order.");
+        }
+        if (Descending)
+        {
+            result = -result;
+        }
+        return result;
+        }
+    }
+
+}☻
